d5841f5e5b747286d38c8c0ee92eedad269df1b4
Jenny Ongele <rosienifer@icloud.com>
2017-06-21 23:09:35 +0000

    COMPLETED MINI-LAB

---
+++ hashiterationlab.rb
@@ -0,0 +1,14 @@
+summer_olympics={
+    :sydney => "2000",
+    :athens => "2004",
+    :beijing => "2008",
+    :london => "2012",
+}
+
+summer_olympics.each do |city, year|
+    puts "The #{city.upcase} Summer olympics took place in #{year}."
+    
+end
+
+
+
